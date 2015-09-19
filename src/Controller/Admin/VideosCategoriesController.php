b9e086fb667440df4c4b8542b17b1b9093c964dc
Mirko Pagliai <mirko.pagliai@gmail.com>
2015-09-19 15:00:55 +0200

    some fixes for controllers

--- src/Controller/Admin/VideosCategoriesController.php
+++ src/Controller/Admin/VideosCategoriesController.php
@@ -1,81 +1,102 @@
 <?php
+/**
+ * This file is part of MeYoutube.
+ *
+ * MeYoutube is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * MeYoutube is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with MeYoutube.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author		Mirko Pagliai <mirko.pagliai@gmail.com>
+ * @copyright	Copyright (c) 2015, Mirko Pagliai for Nova Atlantis Ltd
+ * @license		http://www.gnu.org/licenses/agpl.txt AGPL License
+ * @link		http://git.novatlantis.it Nova Atlantis Ltd
+ */
 namespace MeYoutube\Controller\Admin;
 
-use MeYoutube\Controller\AppController;
+use MeCms\Controller\AppController;
 
 /**
  * VideosCategories controller
  */
 class VideosCategoriesController extends AppController {
 	/**
-     * Lists youtubeVideosCategories
+     * Lists videos categories
      */
     public function index() {
         $this->paginate = [
-            'contain' => ['ParentYoutubeVideosCategories']
+            'contain' => ['ParentVideosCategories']
         ];
-        $this->set('youtubeVideosCategories', $this->paginate($this->YoutubeVideosCategories));
+        $this->set('videosCategories', $this->paginate($this->VideosCategories));
     }
 
     /**
-     * Adds youtube videos category
+     * Adds videos category
      */
     public function add() {
-        $youtubeVideosCategory = $this->YoutubeVideosCategories->newEntity();
+        $videosCategory = $this->VideosCategories->newEntity();
 		
         if($this->request->is('post')) {
-            $youtubeVideosCategory = $this->YoutubeVideosCategories->patchEntity($youtubeVideosCategory, $this->request->data);
+            $videosCategory = $this->VideosCategories->patchEntity($videosCategory, $this->request->data);
 			
-            if($this->YoutubeVideosCategories->save($youtubeVideosCategory)) {
-                $this->Flash->success(__('The youtube videos category has been saved'));
+            if($this->VideosCategories->save($videosCategory)) {
+                $this->Flash->success(__('The videos category has been saved'));
 				return $this->redirect(['action' => 'index']);
             } 
 			else
-                $this->Flash->error(__('The youtube videos category could not be saved'));
+                $this->Flash->error(__('The videos category could not be saved'));
         }
-        $parentYoutubeVideosCategories = $this->YoutubeVideosCategories->ParentYoutubeVideosCategories->find('list', ['limit' => 200]);
+        $parentVideosCategories = $this->VideosCategories->ParentVideosCategories->find('list', ['limit' => 200]);
 
-        $this->set(compact('youtubeVideosCategory', 'parentYoutubeVideosCategories'));
+        $this->set(compact('videosCategory', 'parentVideosCategories'));
     }
 
     /**
      * Edits youtube videos category
-     * @param string $id Youtube Videos Category ID
+     * @param string $id Videos Category ID
      * @throws \Cake\Network\Exception\NotFoundException
      */
     public function edit($id = NULL)  {
-        $youtubeVideosCategory = $this->YoutubeVideosCategories->get($id, [
+        $videosCategory = $this->VideosCategories->get($id, [
             'contain' => []
         ]);
 		
         if($this->request->is(['patch', 'post', 'put'])) {
-            $youtubeVideosCategory = $this->YoutubeVideosCategories->patchEntity($youtubeVideosCategory, $this->request->data);
+            $videosCategory = $this->VideosCategories->patchEntity($videosCategory, $this->request->data);
 			
-            if($this->YoutubeVideosCategories->save($youtubeVideosCategory)) {
-                $this->Flash->success(__('The youtube videos category has been saved'));
+            if($this->VideosCategories->save($videosCategory)) {
+                $this->Flash->success(__('The videos category has been saved'));
                 return $this->redirect(['action' => 'index']);
             } 
 			else
-                $this->Flash->error(__('The youtube videos category could not be saved'));
+                $this->Flash->error(__('The videos category could not be saved'));
         }
-        $parentYoutubeVideosCategories = $this->YoutubeVideosCategories->ParentYoutubeVideosCategories->find('list', ['limit' => 200]);
+        $parentVideosCategories = $this->VideosCategories->ParentVideosCategories->find('list', ['limit' => 200]);
 
-        $this->set(compact('youtubeVideosCategory', 'parentYoutubeVideosCategories'));
+        $this->set(compact('videosCategory', 'parentVideosCategories'));
     }
     /**
      * Deletes youtube videos category
-     * @param string $id Youtube Videos Category ID
+     * @param string $id Videos Category ID
      * @throws \Cake\Network\Exception\NotFoundException
      */
     public function delete($id = NULL) {
         $this->request->allowMethod(['post', 'delete']);
 		
-        $youtubeVideosCategory = $this->YoutubeVideosCategories->get($id);
+        $videosCategory = $this->VideosCategories->get($id);
 		
-        if($this->YoutubeVideosCategories->delete($youtubeVideosCategory))
-            $this->Flash->success(__('The youtube videos category has been deleted'));
+        if($this->VideosCategories->delete($videosCategory))
+            $this->Flash->success(__('The videos category has been deleted'));
         else
-            $this->Flash->error(__('The youtube videos category could not be deleted'));
+            $this->Flash->error(__('The videos category could not be deleted'));
 			
         return $this->redirect(['action' => 'index']);
     }
--- src/Controller/Admin/VideosController.php
+++ src/Controller/Admin/VideosController.php
@@ -1,85 +1,106 @@
 <?php
+/**
+ * This file is part of MeYoutube.
+ *
+ * MeYoutube is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * MeYoutube is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with MeYoutube.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author		Mirko Pagliai <mirko.pagliai@gmail.com>
+ * @copyright	Copyright (c) 2015, Mirko Pagliai for Nova Atlantis Ltd
+ * @license		http://www.gnu.org/licenses/agpl.txt AGPL License
+ * @link		http://git.novatlantis.it Nova Atlantis Ltd
+ */
 namespace MeYoutube\Controller\Admin;
 
-use MeYoutube\Controller\AppController;
+use MeCms\Controller\AppController;
 
 /**
  * Videos controller
  */
 class VideosController extends AppController {
 	/**
-     * Lists youtubeVideos
+     * Lists videos
      */
     public function index() {
         $this->paginate = [
             'contain' => ['Users', 'Youtubes', 'Categories']
         ];
-        $this->set('youtubeVideos', $this->paginate($this->YoutubeVideos));
+        $this->set('youtubeVideos', $this->paginate($this->Videos));
     }
 
     /**
-     * Adds youtube video
+     * Adds video
      */
     public function add() {
-        $youtubeVideo = $this->YoutubeVideos->newEntity();
+        $youtubeVideo = $this->Videos->newEntity();
 		
         if($this->request->is('post')) {
-            $youtubeVideo = $this->YoutubeVideos->patchEntity($youtubeVideo, $this->request->data);
+            $youtubeVideo = $this->Videos->patchEntity($youtubeVideo, $this->request->data);
 			
-            if($this->YoutubeVideos->save($youtubeVideo)) {
-                $this->Flash->success(__('The youtube video has been saved'));
+            if($this->Videos->save($youtubeVideo)) {
+                $this->Flash->success(__('The video has been saved'));
 				return $this->redirect(['action' => 'index']);
             } 
 			else
-                $this->Flash->error(__('The youtube video could not be saved'));
+                $this->Flash->error(__('The video could not be saved'));
         }
-        $users = $this->YoutubeVideos->Users->find('list', ['limit' => 200]);
-        $youtubes = $this->YoutubeVideos->Youtubes->find('list', ['limit' => 200]);
-        $categories = $this->YoutubeVideos->Categories->find('list', ['limit' => 200]);
+        $users = $this->Videos->Users->find('list', ['limit' => 200]);
+        $youtubes = $this->Videos->Youtubes->find('list', ['limit' => 200]);
+        $categories = $this->Videos->Categories->find('list', ['limit' => 200]);
 
         $this->set(compact('youtubeVideo', 'users', 'youtubes', 'categories'));
     }
 
     /**
-     * Edits youtube video
-     * @param string $id Youtube Video ID
+     * Edits video
+     * @param string $id Video ID
      * @throws \Cake\Network\Exception\NotFoundException
      */
     public function edit($id = NULL)  {
-        $youtubeVideo = $this->YoutubeVideos->get($id, [
+        $youtubeVideo = $this->Videos->get($id, [
             'contain' => []
         ]);
 		
         if($this->request->is(['patch', 'post', 'put'])) {
-            $youtubeVideo = $this->YoutubeVideos->patchEntity($youtubeVideo, $this->request->data);
+            $youtubeVideo = $this->Videos->patchEntity($youtubeVideo, $this->request->data);
 			
-            if($this->YoutubeVideos->save($youtubeVideo)) {
-                $this->Flash->success(__('The youtube video has been saved'));
+            if($this->Videos->save($youtubeVideo)) {
+                $this->Flash->success(__('The video has been saved'));
                 return $this->redirect(['action' => 'index']);
             } 
 			else
-                $this->Flash->error(__('The youtube video could not be saved'));
+                $this->Flash->error(__('The video could not be saved'));
         }
-        $users = $this->YoutubeVideos->Users->find('list', ['limit' => 200]);
-        $youtubes = $this->YoutubeVideos->Youtubes->find('list', ['limit' => 200]);
-        $categories = $this->YoutubeVideos->Categories->find('list', ['limit' => 200]);
+        $users = $this->Videos->Users->find('list', ['limit' => 200]);
+        $youtubes = $this->Videos->Youtubes->find('list', ['limit' => 200]);
+        $categories = $this->Videos->Categories->find('list', ['limit' => 200]);
 
         $this->set(compact('youtubeVideo', 'users', 'youtubes', 'categories'));
     }
     /**
-     * Deletes youtube video
-     * @param string $id Youtube Video ID
+     * Deletes video
+     * @param string $id Video ID
      * @throws \Cake\Network\Exception\NotFoundException
      */
     public function delete($id = NULL) {
         $this->request->allowMethod(['post', 'delete']);
 		
-        $youtubeVideo = $this->YoutubeVideos->get($id);
+        $youtubeVideo = $this->Videos->get($id);
 		
-        if($this->YoutubeVideos->delete($youtubeVideo))
-            $this->Flash->success(__('The youtube video has been deleted'));
+        if($this->Videos->delete($youtubeVideo))
+            $this->Flash->success(__('The video has been deleted'));
         else
-            $this->Flash->error(__('The youtube video could not be deleted'));
+            $this->Flash->error(__('The video could not be deleted'));
 			
         return $this->redirect(['action' => 'index']);
     }
