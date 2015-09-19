cded3d6acffe6d3019757e1f33991a812c1d12a4
Mirko Pagliai <mirko.pagliai@gmail.com>
2015-09-19 19:02:22 +0200

    added api

---
+++ API/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","MeYoutube\\Controller\\Admin\\VideosCategoriesController"],["c","MeYoutube\\Controller\\Admin\\VideosController"],["c","MeYoutube\\Model\\Entity\\Video"],["c","MeYoutube\\Model\\Entity\\VideosCategory"],["c","MeYoutube\\Model\\Table\\VideosCategoriesTable"],["c","MeYoutube\\Model\\Table\\VideosTable"],["c","MeYoutube\\Model\\Validation\\VideosCategoryValidator"],["c","MeYoutube\\Model\\Validation\\VideoValidator"],["c","MeYoutube\\Utility\\Youtube"],["c","MeYoutube\\View\\Helper\\MenuHelper"]];
