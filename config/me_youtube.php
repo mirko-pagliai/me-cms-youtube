c2ccbd56c04efa6e0df4e242cee7ea8ef958e5e3
Mirko Pagliai <mirko.pagliai@gmail.com>
2015-11-07 15:04:09 +0100

    fixed configuration

--- config/me_youtube.php
+++ config/me_youtube.php
@@ -2,19 +2,16 @@
 return ['MeYoutube' => [
 	//Videos
 	'video' => [
-		//"Show" options
-		'show' => [
-			//Displays the video author
-			'author' => TRUE,
-			//Displays the video category
-			'category' => TRUE,
-			//Displays the video created datetime
-			'created' => TRUE,
-			//Plays a spot automatically before each video
-			'spot' => TRUE,
-			//Displays the Shareaholic social buttons
-			//Remember you have to set app and site IDs. See `shareaholic.app_id` and `shareaholic.site_id` in the MeCms configuration
-			'shareaholic' => FALSE
-		]
+		//Displays the video author
+		'author' => TRUE,
+		//Displays the video category
+		'category' => TRUE,
+		//Displays the video created datetime
+		'created' => TRUE,
+		//Plays a spot automatically before each video
+		'spot' => TRUE,
+		//Displays the Shareaholic social buttons
+		//Remember you have to set app and site IDs. See `shareaholic.app_id` and `shareaholic.site_id` in the MeCms configuration
+		'shareaholic' => FALSE
 	]
 ]];
