39a8d4c5dcdf2fe8ca95c5833f0621db11c198b2
Mirko Pagliai <mirko.pagliai@gmail.com>
2016-01-02 22:37:01 +0100

    fixed description

--- config/youtube_keys.php
+++ config/youtube_keys.php
@@ -2,8 +2,6 @@
 /**
  * Before using the plugin, you have to get the API key:
  * https://developers.google.com/youtube/registering_an_application
- * 
- * When you have keys, set them below and RENAME THIS FILE in "youtube.php".
  */
 
 return ['Youtube' => [
