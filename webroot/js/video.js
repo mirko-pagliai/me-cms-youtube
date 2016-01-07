ee26b402db8318b81de971a7fc6b435b86e42b5b
Mirko Pagliai <mirko.pagliai@gmail.com>
2016-01-07 23:31:54 +0100

    updated copyright

--- webroot/js/video.js
+++ webroot/js/video.js
@@ -1,7 +1,7 @@
 /*!
  * This file is part of MeYoutube.
  * @author		Mirko Pagliai <mirko.pagliai@gmail.com>
- * @copyright	Copyright (c) 2015, Mirko Pagliai for Nova Atlantis Ltd
+ * @copyright	Copyright (c) 2016, Mirko Pagliai for Nova Atlantis Ltd
  * @license		http://www.gnu.org/licenses/agpl.txt AGPL License
  * @see			https://developers.google.com/youtube/iframe_api_reference
  */
