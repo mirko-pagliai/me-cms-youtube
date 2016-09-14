a37215873db908435a7aea69b6c40595f5675ac3
Mirko Pagliai <mirko.pagliai@gmail.com>
2016-09-14 20:22:19 +0200

    fixed

--- config/me_youtube.php
+++ config/me_youtube.php
@@ -3,7 +3,6 @@
  * Before using the plugin, you have to get the API key:
  * https://developers.google.com/youtube/registering_an_application
  */
-
 return [
     MEYOUTUBE => [
         //Youtube videos
