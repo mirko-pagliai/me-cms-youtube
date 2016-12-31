363060ebd35b1f3375701df2951ddcf32014f936
Mirko Pagliai <mirko.pagliai@gmail.com>
2016-12-31 15:30:34 +0100

    added tests for `AppTable` class

---
+++ tests/Fixture/YoutubeVideosFixture.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * This file is part of me-cms-youtube.
+ *
+ * me-cms-youtube is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * me-cms-youtube is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with me-cms-youtube.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author      Mirko Pagliai <mirko.pagliai@gmail.com>
+ * @copyright   Copyright (c) 2016, Mirko Pagliai for Nova Atlantis Ltd
+ * @license     http://www.gnu.org/licenses/agpl.txt AGPL License
+ * @link        http://git.novatlantis.it Nova Atlantis Ltd
+ * @see         MeCms\Utility\SitemapBuilder
+ */
+namespace MeCmsYoutube\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * YoutubeVideosFixture
+ */
+class YoutubeVideosFixture extends TestFixture
+{
+    /**
+     * Fields
+     * @var array
+     */
+    //@codingStandardsIgnoreStart
+    public $fields = [
+        'id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
+        'user_id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'youtube_id' => ['type' => 'string', 'length' => 20, 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'category_id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'title' => ['type' => 'string', 'length' => 100, 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'subtitle' => ['type' => 'string', 'length' => 150, 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'text' => ['type' => 'text', 'length' => null, 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'precision' => null],
+        'priority' => ['type' => 'integer', 'length' => 1, 'unsigned' => false, 'null' => false, 'default' => '3', 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'active' => ['type' => 'boolean', 'length' => null, 'null' => false, 'default' => '1', 'comment' => '', 'precision' => null],
+        'is_spot' => ['type' => 'boolean', 'length' => null, 'null' => false, 'default' => '0', 'comment' => '', 'precision' => null],
+        'seconds' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => '0', 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'duration' => ['type' => 'string', 'length' => 255, 'null' => false, 'default' => '00:00', 'collate' => 'latin1_swedish_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'created' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
+        'modified' => ['type' => 'datetime', 'length' => null, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null],
+        '_indexes' => [
+            'user_id' => ['type' => 'index', 'columns' => ['user_id'], 'length' => []],
+            'category_id' => ['type' => 'index', 'columns' => ['category_id'], 'length' => []],
+        ],
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
+        ],
+    ];
+    //@codingStandardsIgnoreEnd
+
+    /**
+     * Records
+     * @var array
+     */
+    public $records = [
+        [
+            'id' => 1,
+            'user_id' => 1,
+            'youtube_id' => 'Lorem ipsum dolor ',
+            'category_id' => 1,
+            'title' => 'Lorem ipsum dolor sit amet',
+            'subtitle' => 'Lorem ipsum dolor sit amet',
+            'text' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+            'priority' => 1,
+            'active' => 1,
+            'is_spot' => 1,
+            'seconds' => 1,
+            'duration' => 'Lorem ipsum dolor sit amet',
+            'created' => '2016-12-31 15:05:40',
+            'modified' => '2016-12-31 15:05:40'
+        ],
+    ];
+}
---
+++ tests/TestCase/Model/Table/AppTableTest.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * This file is part of me-cms-youtube.
+ *
+ * me-cms-youtube is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * me-cms-youtube is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with me-cms-youtube.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author      Mirko Pagliai <mirko.pagliai@gmail.com>
+ * @copyright   Copyright (c) 2016, Mirko Pagliai for Nova Atlantis Ltd
+ * @license     http://www.gnu.org/licenses/agpl.txt AGPL License
+ * @link        http://git.novatlantis.it Nova Atlantis Ltd
+ * @see         MeCms\Utility\SitemapBuilder
+ */
+namespace MeCmsYoutube\Test\TestCase\Model\Table;
+
+use Cake\Cache\Cache;
+use Cake\ORM\TableRegistry;
+use Cake\TestSuite\TestCase;
+
+/**
+ * AppTableTest class
+ */
+class AppTableTest extends TestCase
+{
+    /**
+     * @var \MeCmsYoutube\Model\Table\VideosTable
+     */
+    protected $Videos;
+
+    /**
+     * @var \MeCmsYoutube\Model\Table\VideosCategoriesTable
+     */
+    protected $VideosCategories;
+
+    /**
+     * Fixtures
+     * @var array
+     */
+    public $fixtures = [
+        'plugin.me_cms_youtube.youtube_videos',
+        'plugin.me_cms_youtube.youtube_videos_categories',
+    ];
+
+    /**
+     * Setup the test case, backup the static object values so they can be
+     * restored. Specifically backs up the contents of Configure and paths in
+     *  App if they have not already been backed up
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->Videos = TableRegistry::get('MeCmsYoutube.Videos');
+        $this->VideosCategories = TableRegistry::get('MeCmsYoutube.VideosCategories');
+
+        Cache::clear(false, $this->Videos->cache);
+    }
+
+    /**
+     * Teardown any static object changes and restore them
+     * @return void
+     */
+    public function tearDown()
+    {
+        parent::tearDown();
+
+        unset($this->Videos, $this->VideosCategories);
+    }
+    
+    /**
+     * Test for `getList()` method
+     * @test
+     */
+    public function testGetList()
+    {
+        $cacheKey = sprintf('%s_list', $this->VideosCategories->table());
+        $this->assertEquals($cacheKey, 'youtube_videos_categories_list');
+        $this->assertFalse(Cache::read($cacheKey, $this->VideosCategories->cache));
+
+        $list = $this->VideosCategories->getList();
+        $this->assertEquals([
+            2 => 'Another video category',
+            1 => 'First video category',
+            4 => 'Sub sub video category',
+            3 => 'Sub video category',
+        ], $list);
+        $this->assertEquals($list, Cache::read($cacheKey, $this->VideosCategories->cache)->toArray());
+    }
+
+    /**
+     * Test for `getTreeList()` method
+     * @test
+     */
+    public function testGetTreeList()
+    {
+        $cacheKey = sprintf('%s_tree_list', $this->VideosCategories->table());
+        $this->assertEquals($cacheKey, 'youtube_videos_categories_tree_list');
+        $this->assertFalse(Cache::read($cacheKey, $this->VideosCategories->cache));
+
+        $list = $this->VideosCategories->getTreeList();
+        $this->assertEquals([
+            1 => 'First video category',
+            3 => '—Sub video category',
+            4 => '——Sub sub video category',
+            2 => 'Another video category',
+        ], $list);
+        $this->assertEquals($list, Cache::read($cacheKey, $this->VideosCategories->cache)->toArray());
+    }
+}
