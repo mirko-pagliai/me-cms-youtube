b0ad0d2c38d4f42b19a803deae7f658263626ad3
Mirko Pagliai <mirko.pagliai@gmail.com>
2017-02-07 14:52:20 +0100

    fixed class

--- src/View/View/AppView.php
+++ src/View/View/AppView.php
@@ -22,32 +22,25 @@
  */
 namespace MeCmsYoutube\View\View;
 
-use MeCms\View\View\AppView as BaseView;
+use MeCms\View\View\AppView as MeCmsAppView;
 
 /**
  * Application view class
  */
-class AppView extends BaseView
+class AppView extends MeCmsAppView
 {
     /**
-     * Renders a layout. Returns output from _render(). Returns false on
-     *  error. Several variables are created for use in layout
-     * @param string $content Content to render in a view, wrapped by the
-     *  surrounding layout
-     * @param string|null $layout Layout name
-     * @return mixed Rendered output, or false on error
-     * @see http://api.cakephp.org/3.3/class-Cake.View.View.html#_renderLayout
-     * @throws Cake\Core\Exception\Exception
-     * @uses MeCms\View\View\AppView::renderLayout()
+     * Internal method to set some blocks
+     * @return void
      * @uses MeTools\View\Helper\HtmlHelper::meta()
      */
-    public function renderLayout($content, $layout = null)
+    protected function _setBlocks()
     {
-        //Automatically adds the meta tag for RSS videos
+        parent::_setBlocks();
+
+        //Sets the meta tag for RSS posts
         if (config('default.rss_meta')) {
             $this->Html->meta(__d('me_cms_youtube', 'Latest videos'), '/videos/rss', ['type' => 'rss']);
         }
-
-        return parent::renderLayout($content, $layout);
     }
 }
