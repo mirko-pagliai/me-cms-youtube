91cd5fb1ea2e9b905b7242d5bddf0761dd730f72
mirko-pagliai <mirko.pagliai@gmail.com>
2017-05-09 09:36:30 +0200

    fixed code

--- src/Controller/VideosController.php
+++ src/Controller/VideosController.php
@@ -80,7 +80,7 @@ class VideosController extends AppController
                 ->where(['is_spot' => false])
                 ->order([sprintf('%s.created', $this->Videos->getAlias()) => 'DESC']);
 
-            $videos = $this->paginate($query)->toArray();
+            $videos = $this->paginate($query);
 
             //Writes on cache
             Cache::writeMany([
@@ -232,57 +232,62 @@ class VideosController extends AppController
     {
         $pattern = $this->request->getQuery('p');
 
+        //Checks if the pattern is at least 4 characters long
+        if ($pattern && strlen($pattern) < 4) {
+            $this->Flash->alert(__d('me_cms', 'You have to search at least a word of {0} characters', 4));
+
+            return $this->redirect([]);
+        }
+
+        //Checks the last search
+        if ($pattern && !$this->checkLastSearch($pattern)) {
+            $this->Flash->alert(__d(
+                'me_cms',
+                'You have to wait {0} seconds to perform a new search',
+                config('security.search_interval')
+            ));
+
+            return $this->redirect([]);
+        }
+
         if ($pattern) {
-            //Checks if the pattern is at least 4 characters long
-            if (strlen($pattern) >= 4) {
-                if ($this->checkLastSearch($pattern)) {
-                    $this->paginate['limit'] = config('default.records_for_searches');
+            $this->paginate['limit'] = config('default.records_for_searches');
 
-                    $page = $this->request->getQuery('page', 1);
+            $page = $this->request->getQuery('page', 1);
 
-                    //Sets the initial cache name
-                    $cache = sprintf('search_%s', md5($pattern));
+            //Sets the cache name
+            $cache = sprintf('search_%s_limit_%s_page_%s', md5($pattern), $this->paginate['limit'], $page);
 
-                    //Updates the cache name with the query limit and the number of the page
-                    $cache = sprintf('%s_limit_%s', $cache, $this->paginate['limit']);
-                    $cache = sprintf('%s_page_%s', $cache, $page);
+            //Tries to get data from the cache
+            list($videos, $paging) = array_values(Cache::readMany(
+                [$cache, sprintf('%s_paging', $cache)],
+                $this->Videos->cache
+            ));
 
-                    //Tries to get data from the cache
-                    list($videos, $paging) = array_values(Cache::readMany(
-                        [$cache, sprintf('%s_paging', $cache)],
-                        $this->Videos->cache
-                    ));
+            //If the data are not available from the cache
+            if (empty($videos) || empty($paging)) {
+                $query = $this->Videos->find('active')
+                    ->select(['id', 'title', 'text', 'created'])
+                    ->where(['OR' => [
+                        'title LIKE' => sprintf('%%%s%%', $pattern),
+                        'subtitle LIKE' => sprintf('%%%s%%', $pattern),
+                        'text LIKE' => sprintf('%%%s%%', $pattern),
+                    ]])
+                    ->order([sprintf('%s.created', $this->Videos->getAlias()) => 'DESC']);
 
-                    //If the data are not available from the cache
-                    if (empty($videos) || empty($paging)) {
-                        $query = $this->Videos->find('active')
-                            ->select(['id', 'title', 'text', 'created'])
-                            ->where(['OR' => [
-                                'title LIKE' => sprintf('%%%s%%', $pattern),
-                                'subtitle LIKE' => sprintf('%%%s%%', $pattern),
-                                'text LIKE' => sprintf('%%%s%%', $pattern),
-                            ]])
-                            ->order([sprintf('%s.created', $this->Videos->getAlias()) => 'DESC']);
+                $videos = $this->paginate($query);
 
-                        $videos = $this->paginate($query)->toArray();
-
-                        //Writes on cache
-                        Cache::writeMany([
-                            $cache => $videos,
-                            sprintf('%s_paging', $cache) => $this->request->getParam('paging')
-                        ], $this->Videos->cache);
-                    //Else, sets the paging parameter
-                    } else {
-                        $this->request = $this->request->withParam('paging', $paging);
-                    }
-
-                    $this->set(compact('videos'));
-                } else {
-                    $this->Flash->alert(__d('me_cms', 'You have to wait {0} seconds to perform a new search', config('security.search_interval')));
-                }
+                //Writes on cache
+                Cache::writeMany([
+                    $cache => $videos,
+                    sprintf('%s_paging', $cache) => $this->request->getParam('paging')
+                ], $this->Videos->cache);
+            //Else, sets the paging parameter
             } else {
-                $this->Flash->alert(__d('me_cms', 'You have to search at least a word of {0} characters', 4));
+                $this->request = $this->request->withParam('paging', $paging);
             }
+
+            $this->set(compact('videos'));
         }
 
         $this->set(compact('pattern'));
