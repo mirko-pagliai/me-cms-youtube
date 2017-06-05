f7ae05a0a99e6317e6f8aae37f5b826ff9283968
mirko-pagliai <mirko.pagliai@gmail.com>
2017-06-05 14:08:47 +0200

    removed empty line

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -20,7 +20,6 @@
  * @license     http://www.gnu.org/licenses/agpl.txt AGPL License
  * @link        http://git.novatlantis.it Nova Atlantis Ltd
  */
-
 use Cake\Cache\Cache;
 use Cake\Core\Configure;
 use Cake\Network\Exception\InternalErrorException;
